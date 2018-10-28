adf69bd694bbac11b0c167abe259df1da81a38f9
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-27 22:13:20 -0400

    Expanded mutation type

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const { ApolloServer } = require('apollo-server');
 
 
 const typeDefs = `
+type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+}
 type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+}
+
+type Mutation {
+    postPhoto(name: String! description: String): Photo!
 }
 `
 
+var photos = []
+
 const resolvers = {
     Query: {
-        totalPhotos: () => 42
+        totalPhotos: () => photos.length
+    },
+    Mutation: {
+        postPhoto(parent, args) {
+            photos.push(args)
+            return true
+        }
     }
 }
 
